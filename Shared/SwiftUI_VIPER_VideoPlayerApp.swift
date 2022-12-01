b6eddd8305ba396a7ade4da700cc707a01cccf39
Keyur Patel <patelkeyur7279@gmail.com>
2022-12-01 23:02:41 +0530

    Initial Commit

---
+++ Shared/SwiftUI_VIPER_VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VIPER_VideoPlayerApp.swift
+//  Shared
+//
+//  Created by Keyur Patel on 01/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VIPER_VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
