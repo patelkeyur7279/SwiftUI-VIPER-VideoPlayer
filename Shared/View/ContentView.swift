83ab0c09c26bb1086ccf03099894c11896bed6b2
Keyur Patel <patelkeyur7279@gmail.com>
2022-12-04 03:15:36 +0530

    Working on Drag Video Content Inside Player

--- Shared/View/ContentView.swift
+++ Shared/View/ContentView.swift
@@ -21,6 +21,47 @@ struct ContentView: View {
             Spacer()
             
             CustomVideoPlayer(player: viewModel.player)
+//                .onAppear(perform: {
+//                    viewModel.player.currentItem!.videoComposition = AVMutableVideoComposition(asset: viewModel.player.currentItem!.asset,
+//                                                                                        applyingCIFiltersWithHandler: { request in
+//
+////                        let videoComposition = AVMutableVideoComposition()
+////
+////                        guard let videoTrack = viewModel.player.currentItem!.asset.tracks(withMediaType: .video).first else { return }
+////
+////                        let size = CGSize(width: abs(videoTrack.naturalSize.height), height: abs(videoTrack.naturalSize.width))
+////                        videoComposition.renderSize = size
+////
+////                        let videoInstruction = AVMutableVideoCompositionInstruction()
+////                        videoInstruction.timeRange = CMTimeRange(start: .zero, duration: videoTrack.asset!.duration)
+////
+////                        // Setting up the transform to fix scale and rotation
+////                        let transformInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack)
+////                        let translate = CGAffineTransform(translationX: size.width, y: size.height)
+////                        //let rotate = CGAffineTransform(rotationAngle: CGFloat.pi)
+////
+////                        transformInstruction.setTransform(translate, at: .zero)
+////                        videoInstruction.layerInstructions.append(transformInstruction)
+////                        videoComposition.instructions.append(videoInstruction)
+////                        viewModel.player.currentItem!.videoComposition = videoComposition
+//
+//
+////                        if let filter = self.filters[currentFilter]{
+////                            let source = request.sourceImage.clampedToExtent()
+////                            filter.setValue(source, forKey: kCIInputImageKey)
+////                            if filter.inputKeys.contains(kCIInputScaleKey){
+////                                filter.setValue(30, forKey: kCIInputScaleKey)
+////                            }
+////                            let output = filter.outputImage!.cropped(to: request.sourceImage.extent)
+////                            request.finish(with: output, context: nil)
+////                        }
+////                        else{
+////                            request.finish(with: request.sourceImage, context: nil)
+////                        }
+//
+//
+//                    })
+//                })
                 .frame(width: 300, height: 300, alignment: .center)
                 .clipShape(viewModel.currentShape)
                 .overlay(CustomControlsView(viewModel: viewModel, isHideControl: $hideControls)
@@ -32,13 +73,6 @@ struct ContentView: View {
                     DragGesture()
                         .onChanged { gesture in
                             offset = gesture.translation
-                        }
-                        .onEnded { _ in
-                            if abs(offset.width) > 100 {
-                                // remove the card
-                            } else {
-                                offset = .zero
-                            }
                         }
                 )
             
